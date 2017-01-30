144b6ab2be917bf46192b0ce9196c216b2ee431f
Eric Sauer <esauer@redhat.com>
2017-01-29 21:44:29 -0500

    initial springboot restful application

---
+++ src/main/java/hello/HostInfo.java
@@ -0,0 +1,29 @@
+package hello;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.Process;
+import java.lang.Runtime;
+import java.util.Scanner;
+
+public class HostInfo {
+
+  private final String hostname;
+
+  public HostInfo() throws IOException {
+    this.hostname = execReadToString("hostname");
+  }
+
+  public String getHostname() {
+    return hostname;
+  }
+
+  public static String execReadToString(String execCommand) throws IOException {
+      Process proc = Runtime.getRuntime().exec(execCommand);
+      try (InputStream stream = proc.getInputStream()) {
+          try (Scanner s = new Scanner(stream).useDelimiter("\\A")) {
+              return s.hasNext() ? s.next() : "";
+          }
+      }
+  }
+}
---
+++ src/main/java/hello/Welcome.java
@@ -0,0 +1,14 @@
+package hello;
+
+public class Welcome {
+
+  private final String message;
+
+  public Welcome(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
