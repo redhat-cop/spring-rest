1f1997f2d72f3bb94d7fc23183fe33ab8e929798
Andrew Block <andy.block@gmail.com>
2017-12-18 16:58:42 -0600

    Added support for Prometheus Metrics

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,8 +2,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
 @SpringBootApplication
+@EnablePrometheusMetrics
 public class Application {
 
     public static void main(String[] args) {
