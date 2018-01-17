464ecdbebddfb30d226fa0a1706816f045475e39
Michael LaCourse <mlacours@redhat.com>
2018-01-17 11:02:53 -0500

    Add swagger support

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,11 +4,62 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.metrics.export.prometheus.EnablePrometheusMetrics;
 
+import com.google.common.base.Predicate;
+
+import hello.utils.SimpleCORSFilter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import static springfox.documentation.builders.PathSelectors.*;
+import static com.google.common.base.Predicates.*;
+
 @SpringBootApplication
 @EnablePrometheusMetrics
+@EnableSwagger2
+@ComponentScan(basePackageClasses = {
+            GreetingController.class,
+            SimpleCORSFilter.class
+        })
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);    
+    }
+        
+    @Bean
+    public Docket swaggerSpringMvcPlugin() {
+    	System.out.println("PATHS:" + apiPaths());
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
+            .select()
+            .paths(apiPaths())
+            .build();
+    }
+    
+
+    private Predicate<String> apiPaths() {
+        return or(
+                regex("/v2/greeting.*"),
+                regex("/v2/hostinfo.*"),
+                regex("/v2/envinfo.*"),
+                regex("/v2/*")
+        );
+    }    
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("Openshift uncontained.io Sample REST API")
+            .description("A sample REST API to demonstrate Spring Boot Applications in Openshift.")
+            .license("Apache License Version 2.0")
+            .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+            .version("2.0")
+            .build();
     }
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -6,11 +6,12 @@ import java.util.List;
 
 import org.springframework.metrics.instrument.Counter;
 import org.springframework.metrics.instrument.MeterRegistry;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
+@RequestMapping("/v2")
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
@@ -22,20 +23,30 @@ public class GreetingController {
        counter = registry.counter("greeting_counter");
     }
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/greeting" ,  method = RequestMethod.GET)
+    @ApiOperation(value = "Generate a greeting for a given name")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         counter.increment();
         return new Greeting((int)counter.count(),
                             String.format(template, name));
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/" ,  method = RequestMethod.GET)
+    @ApiOperation(value = "Sample REST default path, list operations", nickname = "getGreeting")
     public Welcome welcome() {
       return new Welcome(welcome, paths);
     }
 
-    @RequestMapping("/hostinfo")
+    @RequestMapping(value = "/hostinfo",  method = RequestMethod.GET)
+    @ApiOperation(value = "List host name running the sample application.")
     public HostInfo hostinfo() throws IOException {
       return new HostInfo();
     }
+    
+    @RequestMapping(value = "/envinfo" ,  method = RequestMethod.GET)
+    @ApiOperation(value = "Display environment variables.")
+    public EnvInfo envinfo(@RequestParam(value="filter", defaultValue="*") String filter) throws IOException {
+      return new EnvInfo(filter);
+    }
+    
 }
