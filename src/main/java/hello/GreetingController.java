a3bcbc338b900698092d75c0d328fe830de86dac
Michael LaCourse <mlacours@redhat.com>
2018-01-19 16:56:58 -0500

    Change to version v1

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -15,14 +15,14 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private static final String welcome = "This is my webservice!";
-    private static final List<String> paths = Arrays.asList("/v2/envinfo", "/v2/greeting", "/v2/hostinfo", "/swagger.json");
+    private static final List<String> paths = Arrays.asList("/v1/envinfo", "/v1/greeting", "/v1/hostinfo", "/swagger.json");
     private final Counter counter;
 
     public GreetingController(MeterRegistry registry) {
        counter = registry.counter("greeting_counter");
     }
 
-    @RequestMapping(value = "/v2/greeting" ,  method = RequestMethod.GET)
+    @RequestMapping(value = "/v1/greeting" ,  method = RequestMethod.GET)
     @ApiOperation(value = "Generate a greeting for a given name")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         counter.increment();
@@ -36,13 +36,13 @@ public class GreetingController {
       return new Welcome(welcome, paths);
     }
 
-    @RequestMapping(value = "/v2/hostinfo",  method = RequestMethod.GET)
+    @RequestMapping(value = "/v1/hostinfo",  method = RequestMethod.GET)
     @ApiOperation(value = "List host name running the sample application.")
     public HostInfo hostinfo() throws IOException {
       return new HostInfo();
     }
     
-    @RequestMapping(value = "/v2/envinfo" ,  method = RequestMethod.GET)
+    @RequestMapping(value = "/v1/envinfo" ,  method = RequestMethod.GET)
     @ApiOperation(value = "Display environment variables.")
     public EnvInfo envinfo(@RequestParam(value="filter", defaultValue="*") String filter) throws IOException {
       return new EnvInfo(filter);
