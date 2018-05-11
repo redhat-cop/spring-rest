9017764b45d52143d86156437235908b186b26bb
Your Name <orisdev@gmail.com>
2018-05-11 17:47:41 -0400

    adding get all

--- src/main/java/com/hello/GreetingController.java
+++ src/main/java/com/hello/GreetingController.java
@@ -1,50 +1,63 @@
 package com.hello;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.metrics.instrument.Counter;
 import org.springframework.metrics.instrument.MeterRegistry;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final Counter counter;
+	@Autowired
+	private GreetingRepository respository;
 
-    public GreetingController(MeterRegistry registry) {
-       counter = registry.counter("greeting_counter");
-    }
+	private static final String template = "Hello, %s!";
+	private final Counter counter;
 
-    @RequestMapping(value = "/v1/greeting" ,  method = RequestMethod.GET)
-    @ApiOperation(value = "Generate a greeting for a given name")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        counter.increment();
-        return new Greeting((int)counter.count(),
-                            String.format(template, name));
-    }
+	public GreetingController(MeterRegistry registry) {
+		counter = registry.counter("greeting_counter");
+	}
 
-    @GetMapping("/")
-    public RedirectView index() {
-        return new RedirectView("swagger-ui.html");
-    }
+	@RequestMapping(value = "/v1/greeting", method = RequestMethod.GET)
+	@ApiOperation(value = "Generate a greeting for a given name")
+	public Greeting greeting(@RequestParam(value = "name") String name) {
+		counter.increment();
+		Greeting greeting = new Greeting((int) counter.count(), String.format(template, name));
+		respository.save(greeting);
+		return greeting;
+	}
 
+	@RequestMapping(value = "/v1/allgreetings", method = RequestMethod.GET)
+	@ApiOperation(value = "Returns A List of Greetings")
+	public List<Greeting> allGreetings() {
+		return respository.findAll();
+	}
 
-    @RequestMapping(value = "/v1/hostinfo",  method = RequestMethod.GET)
-    @ApiOperation(value = "List host name running the sample application.")
-    public HostInfo hostinfo() throws IOException {
-      return new HostInfo();
-    }
-    
-    @RequestMapping(value = "/v1/envinfo" ,  method = RequestMethod.GET)
-    @ApiOperation(value = "Display environment variables.")
-    public EnvInfo envinfo(@RequestParam(value="filter", defaultValue="*") String filter) throws IOException {
-      return new EnvInfo(filter);
-    }
-    
+	@GetMapping("/")
+	public RedirectView index() {
+		return new RedirectView("swagger-ui.html");
+	}
+
+	@RequestMapping(value = "/v1/hostinfo", method = RequestMethod.GET)
+	@ApiOperation(value = "List host name running the sample application.")
+	public HostInfo hostinfo() throws IOException {
+		return new HostInfo();
+	}
+
+	@RequestMapping(value = "/v1/envinfo", method = RequestMethod.GET)
+	@ApiOperation(value = "Display environment variables.")
+	public EnvInfo envinfo(@RequestParam(value = "filter", defaultValue = "*") String filter) throws IOException {
+		return new EnvInfo(filter);
+	}
+
 }
