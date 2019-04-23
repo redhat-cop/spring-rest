59f9e5bb705f4b07d7924f00c3196b6fceecc70c
Robert Greathouse <robert.i.greathouse@gmail.com>
2019-04-22 19:34:57 -0500

    Removed all usage of Spring Fox

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import io.swagger.annotations.ApiOperation;
+//import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -19,7 +19,7 @@ public class GreetingController {
     }
 
     @RequestMapping(value = "/v1/greeting" ,  method = RequestMethod.GET)
-    @ApiOperation(value = "Generate a greeting for a given name")
+    //@ApiOperation(value = "Generate a greeting for a given name")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         counter.increment();
         return new Greeting((int)counter.count(),
@@ -33,13 +33,13 @@ public class GreetingController {
 
 
     @RequestMapping(value = "/v1/hostinfo",  method = RequestMethod.GET)
-    @ApiOperation(value = "List host name running the sample application.")
+    //@ApiOperation(value = "List host name running the sample application.")
     public HostInfo hostinfo() throws IOException {
       return new HostInfo();
     }
     
     @RequestMapping(value = "/v1/envinfo" ,  method = RequestMethod.GET)
-    @ApiOperation(value = "Display environment variables.")
+    //@ApiOperation(value = "Display environment variables.")
     public EnvInfo envinfo(@RequestParam(value="filter", defaultValue="*") String filter) throws IOException {
       return new EnvInfo(filter);
     }
